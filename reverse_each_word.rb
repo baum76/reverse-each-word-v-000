06e25aa8f00785b301aad7f5713a709c3048fe8d
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-23 17:42:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string = "Hello there, and how are you?")
   reverse_each_word = string.split
   reverse_each_word.each do |reverse|
     reverse_each_word.map(&:reverse!)
-    return reverse_each_word.join(",")
+    return reverse_each_word.join(" ")
   end
 end
 
